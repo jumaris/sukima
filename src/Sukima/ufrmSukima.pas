e1e748620ca7271b39cd9cc216a6eb5583898d51
WSukima <WSukima@5b389a8c-f942-0410-af58-5b9e58f62b86>
2008-01-06 06:37:57 +0000

    初始化代码

---
+++ src/Sukima/ufrmSukima.pas
@@ -0,0 +1,26 @@
+// $Id$
+
+unit ufrmSukima;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmSukima = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmSukima: TfrmSukima;
+
+implementation
+
+{$R *.dfm}
+
+end.
